316afb8709bf62c51928597c769e5cc9fdacf22f
Felipe Leo <felipeleocavalcante@hotmail.com>
2024-11-23 14:32:26 -0300

    Initial Commit

---
+++ Rexcycle/RexcycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RexcycleApp.swift
+//  Rexcycle
+//
+//  Created by Leo Leo on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RexcycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
