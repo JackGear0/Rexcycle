69e40245963770d0ae48fea33c3521486df05463
PedroCat <pcatundarocha@gmail.com>
2024-11-24 10:46:43 -0300

    fix: biding sheet HomeView

--- Rexcycle/Home/HomeView.swift
+++ Rexcycle/Home/HomeView.swift
@@ -140,7 +140,7 @@ struct HomeView: View {
                                         showingSheet = true
                                     }
                                     .sheet(isPresented: $showingSheet) {
-                                        RedeemVoucherView(voucher: voucher)
+                                        RedeemVoucherView(voucher: voucher, userAuth: $userAuth)
                                     }
                                 }
                             }
