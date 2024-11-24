b5e15e46090679f8b883d7f3226c5ae66d4f7372
Layza Carneiro <layza.mrcarneiro@gmail.com>
2024-11-24 05:24:57 -0300

    add ui

--- Rexcycle/Material/AddMaterial.swift
+++ Rexcycle/Material/AddMaterial.swift
@@ -11,113 +11,194 @@ struct AddMaterial: View {
     @StateObject private var viewModel = MaterialViewModel()
     @State private var selectedUnit: String = "kg"
     
-    @State private var auxMaterial: String = ""
-    @State private var auxObject: String = ""
-    
-    private let materialList = [ //This is the List of values we'll use
-        "Garrafa um",
-        "Garrafa dois",
-        "Garrafa tres"
-    ]
-    
-    private let objectList = [ //This is the List of values we'll use
-        "Vidro 1",
-        "Vidro 2",
-        "Vidro 3"
-    ]
     var body: some View {
-        VStack{
-            Text("1.Escolha a categoria")
-                .bold()
-                .font(.system(size: 25))
-                .padding()
-                .foregroundColor(.darkGreen)
-            List {
-                Picker("Objeto", selection: $viewModel.selectedObject) {
-                    Text("Garrafa").tag("Garrafa")
-                    Text("Embalagem").tag("Embalagem")
-                    Text("Sacola").tag("Sacola")
-                    Text("Latinha").tag("Latinha")
-                    Text("Papel").tag("Papel")
-                }
-                .pickerStyle(MenuPickerStyle())
-                .listRowSeparator(.hidden)
-                .tint(.darkGreen)
+        ZStack {
+            VStack(alignment: .leading) {
+                Text("1. Escolha a categoria")
+                    .font(.title2)
+                    .bold()
                 
-                Picker("Material", selection: $viewModel.selectedMaterial) {
-                    ForEach(materialOptions(for: viewModel.selectedObject), id: \.self) { material in
-                        Text(material).tag(material)
-                    }
-                }
-                .pickerStyle(MenuPickerStyle())
-                .tint(.darkGreen)
-                .onAppear {
-                    if let firstMaterial = materialOptions(for: viewModel.selectedObject).first {
-                        viewModel.selectedMaterial = firstMaterial
-                    }
-                }
-            }
-            .scrollContentBackground(.hidden)
-            .frame(height: 150)
-            Image(.card2)
-            
-            Text("2.Insira a quantidade")
-                .bold()
-                .font(.system(size: 25))
-                .padding()
-                .foregroundColor(.darkGreen)
-            
-            
-            HStack {
-                Text("Quantidade (kg):")
-                    .font(.headline)
-                
-                Picker("Quantidade", selection: $viewModel.selectedQuantity) {
-                    ForEach(0..<101, id: \.self) { value in
-                        Text("\(Double(value), specifier: "%.0f")").tag(Double(value))
-                    }
-                }
-                .pickerStyle(MenuPickerStyle())
-                .tint(.darkGreen)
-                .frame(width: 100)
-                HStack{
-                    Image(systemName: "dollarsign.circle.fill")
-                        .foregroundColor(.lightBrown)
-                    Text("+130 cred.")
-                        .font(.body)
-                        .foregroundColor(.lightBrown)
-                }
-                .padding(.horizontal)
                 HStack {
-                    Image(systemName: "plus.circle.fill")
-                        .foregroundColor(.white)
-                    Text("Adicionar Item")
-                        .foregroundColor(.white)
+                    Text("Material")
+                        .font(.title3)
+                        .foregroundStyle(.accent)
+                    
+                    Spacer()
+                    
+                    Picker("Material", selection: $viewModel.selectedMaterial) {
+                        ForEach(materialOptions(for: viewModel.selectedObject), id: \.self) { material in
+                            Text(material).tag(material)
+                                .font(.title2)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    .tint(.darkGreen)
+                    .onAppear {
+                        if let firstMaterial = materialOptions(for: viewModel.selectedObject).first {
+                            viewModel.selectedMaterial = firstMaterial
+                        }
+                    }
                 }
-                .padding(.leading)
-                .padding()
-                .bold()
-                .foregroundStyle(.red)
-                .background {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .foregroundStyle(.darkGreen)
-                        .padding(.vertical, 5)
+                
+                Text("2. Insira a quantidade")
+                    .font(.title2)
+                    .bold()
+                
+                HStack {
+                    Button {
+                        viewModel.selectedQuantity += 1.0
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .fill(.babyGreen)
+                                .frame(width: 24, height: 24)
+                            Image(systemName: "plus")
+                        }
+                    }
+                    
+                    Text("\(Int(viewModel.selectedQuantity))")
+                        .font(.title2)
+                        .foregroundStyle(.accent)
+                        .frame(width: 37, height: 28)
+                    
+                    Button {
+                        viewModel.selectedQuantity -= 1.0
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .fill(.babyGreen)
+                                .frame(width: 24, height: 24)
+                            Image(systemName: "minus")
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Text("Kg")
+                        .font(.title3)
+                        .foregroundStyle(.accent)
                 }
-            }
-            .overlay {
-                RoundedRectangle(cornerRadius: 25.0)
-                    .stroke(.lightBrown, lineWidth: 2)
-                    .padding(.vertical, 5)
-            }
-            HStack(spacing: 20) {
-                ForEach(0..<10) { _ in
-                    Image(systemName: "circle.fill")
-                        .resizable()
-                        .frame(width: 21.26, height: 21.26)
-                        .foregroundColor(.babyGreen)
-                        .padding(.vertical)
+                .padding(.horizontal, 5)
+                
+                ZStack {
+                    Button {
                         
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 232, height: 44)
+                                .foregroundStyle(.darkGreen)
+                                .cornerRadius(21)
+                                .padding(.trailing, 120)
+                            
+                            HStack {
+                                ZStack {
+                                    Circle()
+                                        .fill(.white)
+                                        .frame(width: 24, height: 24)
+                                    Image(systemName: "plus")
+                                        .foregroundStyle(.darkGreen)
+                                }
+                                Text("Adicionar Item")
+                                    .foregroundStyle(.white)
+                                    .fontWeight(.bold)
+                            }
+                            .padding(.trailing, 120)
+                        }
+                    }
+                    
+                    Rectangle()
+                        .frame(width: 353, height: 44)
+                        .foregroundStyle(.clear)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 21)
+                                .stroke(Color.lightBrown, lineWidth: 1)
+                        )
+                    
+                    HStack {
+                        Spacer()
+                        Image(systemName: "dollarsign.circle.fill")
+                            .foregroundColor(.lightBrown)
+                        HStack(spacing: 3) {
+                            Text("+\(Int(viewModel.credits))")
+                                .fontWeight(.bold)
+                                .font(.body)
+                            Text("créd.")
+                                .font(.caption)
+                        }
+                        .foregroundColor(.lightBrown)
+                    }
+                    .padding()
                 }
+                .padding(.top, 10)
+                
+                HStack(spacing: 18) {
+                    ForEach(0..<10) { _ in
+                        Image(systemName: "circle.fill")
+                            .resizable()
+                            .frame(width: 21.26, height: 21.26)
+                            .foregroundColor(.babyGreen)
+                            .padding(.vertical)
+                        
+                    }
+                }
+                .padding(.vertical, 10)
+                
+                Text("Resumo")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.darkGreen)
+                    .padding(.horizontal, 144)
+                    .padding(.vertical, 10)
+                
+                HStack {
+                    // adicionados
+                }
+                
+                Divider()
+                    .frame(height: 1)
+                    .background(Color.darkGreen)
+                    .padding(.top, 30)
+                
+                HStack {
+                    Text("0 Item")
+                        .foregroundStyle(.darkGreen)
+                    Text("0 Kg")
+                        .foregroundStyle(.darkGreen)
+                    
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("\(Int(viewModel.credits))")
+                        Text("créditos")
+                    }
+                    .foregroundStyle(.lightBrown)
+                }
+                .fontWeight(.semibold)
+                
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 232, height: 44)
+                            .foregroundColor(.lightBrown)
+                            .cornerRadius(21)
+                        
+                        HStack(alignment: .center) {
+                            Image(systemName: "dollarsign.circle.fill")
+                                .foregroundColor(.white)
+                            
+                            Text("Resgatar Créditos")
+                                .foregroundStyle(.white)
+                                .font(.body)
+                                .fontWeight(.bold)
+                        }
+                        
+                    }
+                }
+                .padding(.top, 20)
+                .padding(.horizontal, 70)
+                
             }
             .padding()
         }
