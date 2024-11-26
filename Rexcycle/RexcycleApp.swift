e98ea40c1f9f237b7ce06f1c928eb03ce1f901b4
Felipe Leo <felipeleocavalcante@hotmail.com>
2024-11-26 09:28:46 -0300

    feat: finish login page

--- Rexcycle/RexcycleApp.swift
+++ Rexcycle/RexcycleApp.swift
@@ -21,63 +21,71 @@ struct RexcycleApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationStack {
-                if !isLoggedIn {
-                    VStack(spacing: 60) {
-                        Button(action: {
-                            Task {
-                                do {
-                                    try await API.login(username: "Pedro", password: "123456")
-                                    userAuth = try await API.me()
-                                    isLoggedIn = true
-                                    print("Login realizado com sucesso!")
-                                } catch {
-                                    print("Erro ao fazer login: \(error)")
+                ZStack{
+                    Color.babyGreen
+                    if !isLoggedIn {
+                        VStack(spacing: 20) {
+                            Image(.icon)
+                                .font(.system(size: 50))
+                                .padding(.bottom, 30)
+                            Button(action: {
+                                Task {
+                                    do {
+                                        try await API.login(username: "Pedro", password: "123456")
+                                        userAuth = try await API.me()
+                                        isLoggedIn = true
+                                        print("Login realizado com sucesso!")
+                                    } catch {
+                                        print("Erro ao fazer login: \(error)")
+                                    }
                                 }
+                            })
+                            {
+                                Text("Login como transportadora")
+                                    .font(.title)
+                                    .foregroundStyle(.white)
+                                    .padding()
+                                    .background(.darkGreen)
+                                    .cornerRadius(8)
                             }
-                        }) {
-                            Text("Login como transportadora")
-                                .font(.title)
-                                .foregroundStyle(.black)
-                                .padding()
-                                .background(.babyGreen)
-                                .cornerRadius(8)
-                        }
-                        
-                        Button(action: {
-                            Task {
-                                do {
-                                    try await API.login(username: "Apple", password: "123456")
-                                    userAuth = try await API.me()
-                                    isLoggedIn = true
-                                    print("Login realizado com sucesso!")
-                                } catch {
-                                    print("Erro ao fazer login: \(error)")
+                            
+                            Button(action: {
+                                Task {
+                                    do {
+                                        try await API.login(username: "Apple", password: "123456")
+                                        userAuth = try await API.me()
+                                        isLoggedIn = true
+                                        print("Login realizado com sucesso!")
+                                    } catch {
+                                        print("Erro ao fazer login: \(error)")
+                                    }
                                 }
+                            }) {
+                                Text("Login como empresa")
+                                    .font(.title)
+                                    .foregroundStyle(.white)
+                                    .padding()
+                                    .background(.darkGreen)
+                                    .cornerRadius(8)
                             }
-                        }) {
-                            Text("Login como empresa")
-                                .font(.title)
-                                .foregroundStyle(.black)
-                                .padding()
-                                .background(.babyGreen)
-                                .cornerRadius(8)
                         }
-                    }
-                } else {
-                    TabView {
-                        HomeView(userAuth: userAuth!)
-                            .tabItem {
-                                Image(systemName: "leaf.fill")
-                                Text("Home")
-                            }
-                        
-                        ChatListView()
-                            .tabItem {
-                                Image(systemName: "message.fill")
-                                Text("Chat")
-                            }
+                    } else {
+                        TabView {
+                            HomeView(userAuth: userAuth!)
+                                .tabItem {
+                                    Image(systemName: "leaf.fill")
+                                    Text("Home")
+                                }
+                            
+                            ChatListView()
+                                .tabItem {
+                                    Image(systemName: "message.fill")
+                                    Text("Chat")
+                                }
+                        }
                     }
                 }
+                .ignoresSafeArea()
             }
             .accentColor(.darkGreen)
             .onAppear {
